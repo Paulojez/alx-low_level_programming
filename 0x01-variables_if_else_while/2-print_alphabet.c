252cfa909919af631978973c8d92a104631f21e9
paulojez <dafloxy@gmail.com>
2022-06-03 14:32:47 -0700

    hot fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - prints the alphabet in lowercase,
@@ -16,5 +14,6 @@ int main(void)
 putchar (ch);
 }
 putchar ('\n');
+
 return (0);
 }
